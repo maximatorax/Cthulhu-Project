7b20a1c2be11ddd6d8dbf05125ae6499fc2b8e64
Maxime <maxime_morette@hotmail.com>
2020-04-17 11:55:08 -0400

    Base assets + playercontroller + cameracontroller + animations

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Animator playerAnimator;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController charController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+        playerAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (charController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+            if (moveDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(moveDirection);
+            }
+            playerAnimator.SetFloat("VerticalSpeed", moveDirection.z);
+            playerAnimator.SetFloat("HorizontalSpeed", moveDirection.x);
+            if (Input.GetButton("Jump"))
+            {
+                playerAnimator.SetTrigger("Jump");
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        charController.Move(moveDirection * Time.deltaTime);
+    }
+}
